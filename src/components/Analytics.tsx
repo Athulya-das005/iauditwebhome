68eb369f75e74099266271bcf85b2fab65cfc421
Athulya das <athulyadas005@gmail.com>
2026-03-19 23:09:40 +0530

    Update: again gt and linkedin issue

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -86,39 +86,38 @@ export default function Analytics() {
             )}
 
             {/* ========== LinkedIn Insight Tag ========== */}
-            {linkedinPartnerId && (
-                <>
-                    <Script id="linkedin-partner-init" strategy="afterInteractive">
-                        {`
-                            _linkedin_partner_id = "${linkedinPartnerId}";
-                            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-                            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-                        `}
-                    </Script>
-                    <Script id="linkedin-insight-loader" strategy="afterInteractive">
-                        {`
-                            (function(l) {
-                                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-                                window.lintrk.q=[]}
-                                var s = document.getElementsByTagName("script")[0];
-                                var b = document.createElement("script");
-                                b.type = "text/javascript";b.async = true;
-                                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-                                s.parentNode.insertBefore(b, s);
-                            })(window.lintrk);
-                        `}
-                    </Script>
-                    <noscript>
-                        <img
-                            height="1"
-                            width="1"
-                            style={{ display: "none" }}
-                            src={`https://px.ads.linkedin.com/collect/?pid=${linkedinPartnerId}&fmt=gif`}
-                            alt=""
-                        />
-                    </noscript>
-                </>
-            )}
+            <Script id="linkedin-partner-init" strategy="afterInteractive">
+                {`
+_linkedin_partner_id = "9714209";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+                `}
+            </Script>
+            <Script id="linkedin-insight-loader" strategy="afterInteractive">
+                {`
+(function(l) {
+if (!l){
+  window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+  window.lintrk.q=[];
+}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";
+b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);
+})(window.lintrk);
+                `}
+            </Script>
+            <noscript>
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: "none" }}
+                    src="https://px.ads.linkedin.com/collect/?pid=9714209&fmt=gif"
+                    alt=""
+                />
+            </noscript>
 
             {/* ========== Google Tag Manager (GTM) ========== */}
             {gtmId && (
